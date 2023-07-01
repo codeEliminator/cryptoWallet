7ac947aa37935a70cd238db9663b88da607b5d67
Artem Arutiunian <whoami@MacBook-Air-Artem.local>
2023-07-02 00:42:36 +0100

    idiot mistake i dont know what to do special commit for git checkout

--- Components/Navigation/Navigation.js
+++ Components/Navigation/Navigation.js
@@ -9,71 +9,60 @@ import Icon from 'react-native-vector-icons/Ionicons';
 const Tab = createBottomTabNavigator();
 
 const NavigationBar = () => {
-    return(
-        <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
+  return(
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
             headerShown: false,
             tabBarStyle: {
-              position: 'absolute',
-              bottom: 0,
-              right: 0,              
-              height: 100,
-              width: "100%",              
-              ...styles.tabBarStyle
+            position: 'absolute',
+            bottom: 0,
+            right: 0,              
+            height: 100,
+            width: "100%",              
+            ...styles.tabBarStyle
             },
             tabBarShowLabel: false
-          }}
+        }}
         >
-          <Tab.Screen options={{
-              tabBarIcon:({focused}) => (
-                <View style = {{alignItems: 'center'}}>
+        <Tab.Screen options={{
+          tabBarIcon:({focused}) => (
+              <View style = {{alignItems: 'center'}}>
                   <Icon 
-                    name={focused ? 'home' : 'home-outline'} 
-                    size={20} 
-                    color={focused ? '#fff' : '#DCDCDC'} 
+                      name={focused ? 'home' : 'home-outline'} 
+                      size={20} 
+                      color={focused ? '#fff' : '#DCDCDC'} 
                   />
                   <Text style={{color: focused ? '#fff' : '#DCDCDC'}}>Home</Text>
-                </View>
-              )
-          }} name="Home" >
-            {() => (
-                <View>
-                    <Home />
-                </View>
-            )}
-          </Tab.Screen>
-          <Tab.Screen options={{
+              </View>
+            )
+        }} name="Home" component={Home} />
+        <Tab.Screen options={{
             tabBarIcon:({focused}) => (
-              <View style = {{alignItems: 'center'}}>
+            <View style = {{alignItems: 'center'}}>
                 <Icon 
-                  name={focused ? 'search' : 'search-outline'} 
-                  size={20} 
-                  color={focused ? '#fff' : '#DCDCDC'} 
+                name={focused ? 'search' : 'search-outline'} 
+                size={20} 
+                color={focused ? '#fff' : '#DCDCDC'} 
                 />
                 <Text style={{color: focused ? '#fff' : '#DCDCDC'}}>Search</Text>
-              </View>
-            )}} name="Search" >
-                {() => (
-                    <View>
-                        <Search />
-                    </View>
-                )}
-            </Tab.Screen>
-        </Tab.Navigator>
-      </NavigationContainer>
-    )
+            </View>
+            )}} name="Search" component={Search} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
+  )
 }
 
 const styles = StyleSheet.create({
-    tabBarStyle: {
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-        backgroundColor: 'hsl(180, 100%, 20%)'
-    }
+  tabBarStyle: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    backgroundColor: 'hsl(180, 100%, 20%)'
+  }
 })
 
 export default NavigationBar
